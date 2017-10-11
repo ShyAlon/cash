8c06c050797d17ea7bb12a77e6a99e80e906d44c
Shy Alon <shy@sidekick.im>
2017-10-11 18:29:19 +0300

    start

---
+++ main.py
@@ -0,0 +1,7 @@
+from tsne import Reducer
+
+if __name__ == '__main__':
+# Copy process from matlab
+# 
+    r = Reducer()  
+    res = r.process()
--- tsne.py
+++ tsne.py
@@ -1,5 +1,12 @@
 import numpy as np
 from sklearn.manifold import TSNE
-X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-X_embedded = TSNE(n_components=2).fit_transform(X)
-X_embedded.shape
+
+class Reducer:
+    def __init__(self):
+         self.r = 0
+         self.i = 0
+
+    def process(self):
+        X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+        X_embedded = TSNE(n_components=2).fit_transform(X)
+        return X_embedded.shape
